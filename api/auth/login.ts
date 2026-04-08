6dfb6b7c7d2b1591b7b1e62fb3f94fa68477e689
Unwickbr <fariavictor@live.com>
2026-04-07 22:38:04 -0300

    Remove deprecated email auth routes

--- api/auth/login.ts
+++
@@ -1,8 +0,0 @@
-import type { ApiRequest, ApiResponse } from "../../server/types.js";
-import { sendJson } from "../../server/http.js";
-
-export default async function handler(_req: ApiRequest, res: ApiResponse) {
-  return sendJson(res, 410, {
-    error: "Login por e-mail e senha foi desativado. Use sua conta Google.",
-  });
-}
